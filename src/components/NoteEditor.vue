ab0366e5d97f9defd03aac86913b4a3e9b8bf770
Mike <mikehammond32@gmail.com>
2019-08-15 21:13:31 -0400

    created noteselectors component

--- src/components/NoteEditor.vue
+++ src/components/NoteEditor.vue
@@ -1,16 +1,16 @@
-<template>  
-     <div class="note-editor">
-       <p class="note-editor-info">Timestamp here...</p>
-       <textarea class="note-editor-input">
-         First note...
-        
-         Note text here...
-       </textarea>
-     </div>
-</template> 
+<template>
+  <div class="note-editor">
+    <p class="note-editor-info">Timestamp here...</p>
+    <textarea class="note-editor-input">
+      First note...
 
-<script>
+      Note text here...
+    </textarea>
+  </div>
+</template>
+
+<script type="text/javascript">
 export default {
-  name: 'noteeditor'
-};
+  name: 'note-editor'
+}
 </script>
